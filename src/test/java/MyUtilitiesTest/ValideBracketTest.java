5db3de4b1ac1997cbddd0ad25cc3e3838c01bc11
WasanMoosa <wasssoon@gmail.com>
2023-01-11 11:16:21 +0400

    all TDD has been solved

--- src/test/java/MyUtilitiesTest/ValideBracketTest.java
+++ src/test/java/MyUtilitiesTest/ValideBracketTest.java
@@ -8,27 +8,24 @@ import org.junit.jupiter.api.Test;
 class ValideBracketTest {
 
 	@Test
-	@DisplayName (" Take null  as an arguments.")
+	@DisplayName(" Take null  as an arguments.")
 	void test1() {
 		ValideBracket valideBracket = new ValideBracket();
-		assertThrows(NullPointerException.class,()-> valideBracket.validParantheses(null));
+		assertThrows(NullPointerException.class, () -> valideBracket.validParantheses(null));
 	}
-	
-	
+
 	@Test
-	@DisplayName (" Take an empty string as an argument.")
+	@DisplayName(" Take an empty string as an argument.")
 	void test2() {
 		ValideBracket valideBracket = new ValideBracket();
 		assertEquals(true, valideBracket.validParantheses(""));
 	}
-	
+
 	@Test
-	@DisplayName ("Can throw an exception.")
-	void test3() throws Exception{
+	@DisplayName("Can throw an exception.")
+	void test3() throws Exception {
 		ValideBracket valideBracket = new ValideBracket();
-		assertThrows(NullPointerException.class,()-> valideBracket.validParantheses(null));
+		assertThrows(NullPointerException.class, () -> valideBracket.validParantheses(null));
 	}
-
-
 
 }
